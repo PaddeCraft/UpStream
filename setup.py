68709df164cc679c9e8e7e17cbaeb4dbd564984d
PaddeCraft <70023807+PaddeCraft@users.noreply.github.com>
2022-04-22 14:52:01 +0200

    Fix dependencies and make ready for next release

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     description="UpDog-like client with a design and features for the 21th century.",
     author="PaddeCraft",
     packages=["upstream"],
-    install_requires=["rich", "flask", "typer"],
-    dependency_links=["https://api.github.com/repos/PaddeCraft/Log4py/zipball"],
+    install_requires=["rich", "flask", "typer", "log4py @ https://api.github.com/repos/PaddeCraft/Log4py/zipball"],
     include_package_data=True,
 )
